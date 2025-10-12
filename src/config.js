4f27390132cceb81183badaba43f459c00f89367
Carl Corneil <aecorn@gmail.com>
2025-10-12 23:14:10 +0200

    Added IMDB link

--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@ export const SITE = {
     githubUser: "aecorn",
     linkedinUrl: "https://www.linkedin.com/in/carl-corneil-4312098/",
     farmUrl: "https://www.skogensverksted.no",
+    imdbUrl: "https://www.imdb.com/name/nm3015785/",
     email: "aecorn@gmail.com",
     blogPath: "blog",
     projectsPath: "projects"
